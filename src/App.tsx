3fe7e00cca4dba9340cce6d611caa2d9c2129fdd
Tikhon Moskvin <tihonoskvin@mail.ru>
2023-07-01 16:55:31 +0300

    lesha

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <div>Lesha is developer</div>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
